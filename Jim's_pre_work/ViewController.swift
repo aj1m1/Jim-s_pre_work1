8015ac9030bdea7021ab9422225e4ede26c793dd
Jim Appiah <ajim@ad.unc.edu>
2022-01-01 17:46:42 -0500

    Initial Commit

---
+++ Jim's_pre_work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jim's_pre_work
+//
+//  Created by Jim Appiah on 1/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
